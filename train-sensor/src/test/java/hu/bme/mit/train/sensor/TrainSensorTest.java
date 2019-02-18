15602f58f68862b5cde574decdbfb41334b07793
Szilard1997 <47691576+Szilard1997@users.noreply.github.com>
2019-02-18 07:25:55 -0800

    Update TrainSensorTest.java

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -1,6 +1,7 @@
 package hu.bme.mit.train.sensor;
 
-import org.junit.Assert.*;
+import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
