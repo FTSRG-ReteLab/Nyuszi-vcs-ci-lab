69b326a0f5c1031dd7e76390c26ae0f59525510e
Szilard1997 <kicsitoni11@gmail.com>
2019-04-08 07:13:02 -0700

    jacoco

--- train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
+++ train-sensor/src/main/java/hu/bme/mit/train/sensor/TrainSensorImpl.java
@@ -24,10 +24,9 @@ public class TrainSensorImpl implements TrainSensor {
 	public void overrideSpeedLimit(int speedLimit) {
 		if (speedLimit < 0 || speedLimit > 500 || speedLimit < this.speedLimit / 2) {
 			user.setAlarmState(true);
-		} else {
+		}
 			this.speedLimit = speedLimit;
 			controller.setSpeedLimit(speedLimit);
-		}
 	}
 
 }
